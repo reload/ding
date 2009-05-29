72e7870b750de560f20224cef9f9ffbe1677056a
Mikkel Hoegh <mikkel@hoegh.org>
2009-05-29 11:31:14 +0200

    Added Drupal content search to our custom search.

--- sites/all/modules/ting/js/ting_search_result_page.js
+++ sites/all/modules/ting/js/ting_search_result_page.js
@@ -4,18 +4,35 @@
  * JavaScript helper for the Ting search result page.
  */
 
-Drupal.behaviors.tingSearchResultPage = function () {
+$(document).ready(function() {
+  Drupal.tingSearch = {};
+
   // Put the search keys into the main searchbox.
   $("#block-ting-searchbox :text").val(Drupal.settings.tingSearch.keys);
 
   // Remove the original output from the search module, and make room
   // for our search results and the facet browser.
-  $("#content-inner").html('<div id="ding-facet-browser"></div><div id="ting-search-result">Søger…</div>');
+  $("#content-inner").html('<ul id="ting-search-tabs"><li class="ting">Ting</li><li class="content">Content</li></ul><div id="ding-facet-browser"></div><div id="ting-search-result">Søger…</div>').end();
 
   // Search Ting via Kaspers methods.
-  $.getJSON(Drupal.settings.tingSearch.url, {'query': 'dc.title:' + Drupal.settings.tingSearch.keys}, function (data) {
+  $.getJSON(Drupal.settings.tingSearch.ting_url, {'query': 'dc.title:' + Drupal.settings.tingSearch.keys}, function (data) {
+    Drupal.tingSearch.tingData = data;
     Drupal.dingTingFacetBrowser("#ding-facet-browser", data);
-
   });
-}
+
+  // Search Drupals content.
+  $.getJSON(Drupal.settings.tingSearch.content_url, {'query': Drupal.settings.tingSearch.keys}, function (data) {
+    Drupal.tingSearch.contentData = data;
+    var tab = $("#ting-search-tabs li.content");
+    tab.append(' <span>(' + data.count + ')</span>');
+    if (data.count) {
+      tab.html('<a href="#">' + tab.html() + '</a>');
+      tab.click(function (eventObject) {
+        $("#ding-facet-browser").hide();
+        $("#ting-search-result").html(Drupal.tingSearch.contentData.result_html);
+        return false;
+      });
+    }
+  });
+});
 
