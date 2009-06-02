1e0557555886ba17d7125c2e8a1ba65f936b0600
Mikkel Hoegh <mikkel@hoegh.org>
2009-06-02 14:20:14 +0200

    Office hours bugfix [#106 state:resolved]

--- sites/all/modules/contrib/office_hours/office_hours.js
+++ sites/all/modules/contrib/office_hours/office_hours.js
@@ -12,19 +12,20 @@
 Drupal.behaviors.officeHours = function () {
   var nid_matcher = new RegExp("node-(\\d+)");
   $(".office-hours-week:not(.oh-processed)").each(function () {
-    $(this);
     var nid = nid_matcher.exec($(this).attr('class'))[1];
-    $(this).addClass('oh-processed')
-    .find('.week-info')
-    .prepend('<a class="prev" href="#">&#9664;</a>')
-    .append('<a class="next" href="#">&#9654;</a>')
-    .end()
-    .find('a.prev')
-    .click(function () {return Drupal.officeHours.changeWeek(nid, 'prev');})
-    .end()
-    .find('a.next')
-    .click(function () {return Drupal.officeHours.changeWeek(nid, 'next');})
-    .end();
+    if (Drupal.settings.officeHours.hasOwnProperty("n" + nid)) {
+      $(this).find('.week-info')
+        .prepend('<a class="prev" href="#">&#9664;</a>')
+        .append('<a class="next" href="#">&#9654;</a>')
+      .end()
+      .find('a.prev')
+        .click(function () {return Drupal.officeHours.changeWeek(nid, 'prev');})
+      .end()
+      .find('a.next')
+        .click(function () {return Drupal.officeHours.changeWeek(nid, 'next');})
+      .end();
+    };
+    $(this).addClass('oh-processed');
   });
 };
 
@@ -33,7 +34,7 @@ Drupal.officeHours = {};
 
 // Change view to another week.
 Drupal.officeHours.changeWeek = function (nid, direction) {
-  var conf = Drupal.settings.officeHours[nid];
+  var conf = Drupal.settings.officeHours["n" + nid];
   if (direction == 'prev') {
     var week = parseInt(conf.week) - 1;
   }
@@ -42,8 +43,8 @@ Drupal.officeHours.changeWeek = function (nid, direction) {
   }
 
   $.getJSON(conf.callback + '/' + nid + '/' + conf.field_name + '/' + conf.year + '/' + week, {}, function (data, textStatus) {
-    Drupal.settings.officeHours[data.nid].week = parseInt(data.week);
-    Drupal.settings.officeHours[data.nid].year = parseInt(data.year);
+    Drupal.settings.officeHours["n" + data.nid].week = parseInt(data.week);
+    Drupal.settings.officeHours["n" + data.nid].year = parseInt(data.year);
 
     $('.office-hours-week.node-' + nid).slideUp('normal', function () {
       $(this).replaceWith(data.html);
