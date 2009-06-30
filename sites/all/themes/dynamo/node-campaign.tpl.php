07461038096cb214014f0fc40ae05d8c664beb72
mortendk <mdk@bookroyale.local>
2009-07-01 00:01:29 +0200

    footer blocks, comments & edits etc

--- sites/all/themes/dynamo/node-campaign.tpl.php
+++ sites/all/themes/dynamo/node-campaign.tpl.php
@@ -1,27 +1,9 @@
-<?php // dsm($node);	?>	
-<?php  //dsm(get_defined_vars());  ?> 
-<?php //print $FIELD_NAME_rendered ?>
-
-
-<div class="clearfix campaign campaign-theme-<?php print $node->campaign_theme;?> campaign-type-<?php print $node->campaign_type;?>">
-
-<?php if ($node->campaign_type == "image-only") { ?>
-    <?php
-      print l($node->field_campaign_image['0']['view'], $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE));
-   ?>
-
-<?php }else{ ?>
-
-  <?php if($node->title){	?>	
-    <h3><?php print l($node->title, 'node/'.$node->nid); ?></h3>
-  <?php } ?>
-
-<?php
-    print l($node->teaser,  $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE));
-
-?>  
-
-
-<?php } ?>
-
-  </div>
+<div class="campaign campaign-theme-<?php print $node->campaign_theme;?> campaign-type-<?php print $node->campaign_type;?> clearfix">
+<?php 
+if ($node->campaign_type == "image-only") { 
+  print l($node->field_campaign_image['0']['view'], $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE));
+}else{ 
+  print l(filter_xss($node->content['body']['#value']),  $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE));
+} 
+?>
+</div>
--- sites/all/themes/dynamo/views-view-fields--article-list.tpl.php
+++ sites/all/themes/dynamo/views-view-fields--article-list.tpl.php
@@ -1,33 +1,34 @@
 <?php
-  //dsm($fields);
+  dsm($fields);
 ?>
 
 <div class="subject"><?php print $fields['tid']->content; ?> </div>
 <h2><?php print $fields['title']->content; ?></h2>
 <div class="meta">
+  <?php if($fields['field_library_ref_nid']->content){ ?>
   <ul>
     <li><?php print $fields['field_library_ref_nid']->content; ?></li>
   </ul>
+  <?php } ?>
   
-<?php print $fields['created']->content; ?>
-  <i>af</i> 
+  <?php print $fields['created']->content; ?>
+  <i><?php print t('by'); ?></i>
+  
   <span class="author">
     <?php print $fields['name']->content; ?>  
   </span>
-
-    <?php 
-      if($fields['comment_count']->raw >= "1"){
-        print "(". $fields['comment_count']->content .")";
-      }  
-    ?>
-
-
-  <?php print $fields['edit_node']->content; ?>
+  <?php 
+    if($fields['comment_count']->raw >= "1"){
+      print "(". $fields['comment_count']->content .")";
+    }  
+  ?>
 </div>
+
 <p>
   <?php print $fields['field_teaser_value']->content; ?>  
   
   <?php print $fields['body']->content; ?>    
-
+  <?php print $fields['edit_node']->content; ?>
   <span class="more-link"><?php print $fields['view_node']->content; ?></span>
+
 </p>
