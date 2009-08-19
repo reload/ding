030baf12ce10d9e7f630035dc3c409fac0244899
mortendk <mortendk@book-royale-II.local>
2009-08-19 16:28:57 +0200

    kampagneboxe FTW!

--- sites/all/themes/dynamo/node-campaign.tpl.php
+++ sites/all/themes/dynamo/node-campaign.tpl.php
@@ -1,9 +1,16 @@
-<div class="campaign campaign-theme-<?php print $node->campaign_theme;?> campaign-type-<?php print $node->campaign_type;?> clearfix">
-<?php 
-if ($node->campaign_type == "image-only") { 
-  print l($node->field_campaign_image['0']['view'], $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE));
-}else{ 
-  print l(filter_xss($node->content['body']['#value']),  $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE));
-} 
-?>
+<?php if ($node->campaign_type == "image-only") { ?>		
+	<div class="campaign-image campaign-type-<?php print $node->campaign_type;  ?> clearfix">
+		<?php print l($node->field_campaign_image['0']['view'], $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE)); ?>
+	</div>
+<?php }else{ ?>
+	<div class="campaign-text clearfix">
+		<div class="campaign-inner">
+			<div class="campaign-type-<?php print $node->campaign_type;?>">
+				<div class="campaign-theme campaign-theme-<?php print $node->campaign_theme; ?>">
+					<?php print l(filter_xss($node->content['body']['#value']),  $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE)); ?>
+				</div>
+			</div>
+		</div>
 </div>
+
+<?php } ?>
