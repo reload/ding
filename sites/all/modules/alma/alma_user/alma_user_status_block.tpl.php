980431aa2499bcd550f47639b80b59f61a0af48f
Mikkel Hoegh <mikkel@hoegh.org>
2009-08-20 16:56:37 +0200

    Implemented user loan status.
    Fixes [#303] apart from the styling.

--- sites/all/modules/alma/alma_user/alma_user_status_block.tpl.php
+++ sites/all/modules/alma/alma_user/alma_user_status_block.tpl.php
@@ -10,21 +10,27 @@
 
 <div class="welcome-username">
   <span class="welcome"><?php print t('Welcome'); ?></span>
-  <span class="user-name"><?php print l($user->name, 'patron/profile'); ?></span>
+  <span class="user-name"><?php print l($display_name, 'user/' . $user->uid . '/profile'); ?></span>
 </div>
 
-<div class="alma-patron-status">
+<div class="alma-patron-status <?php print $wrapper_classes; ?>">
+<?php if (!$user_status_unavailable): ?>
   <div class="cart-status">
     <span class="count"><?php print $cart_count; ?></span>
     <?php print l('Go to cart', 'patron/cart'); ?>
   </div>
   <ul class="materials">
-    <li class="borrowed">
-      <?php print l(t("Borrowed materials") . ' <span class="count">' . $borrowed_count . '</span>', 'patron/status', array('html' => TRUE)); ?>
+    <li class="loans">
+      <?php print l(t("Loans") . ' <span class="count">' . $user_status['loan_count'] . '</span>', 'user/'. $user->uid . '/status', array('html' => TRUE)); ?>
     </li>
     <li class="reservations">
-      <?php print l(t("Reservations") . ' <span class="count">' . $reservation_count . '</span>', 'patron/status', array('html' => TRUE)); ?>
+      <?php print l(t("Reservations") . ' <span class="count">' . $user_status['reservation_count'] . '</span>', 'user/'. $user->uid . '/status', array('html' => TRUE)); ?>
     </li>
   </ul>
+<?php else: ?>
+  <div class="status-unavailable">
+    <?php print $status_unavailable_message; ?>
+  </div>
+<?php endif; ?>
 </div>
 
