37723e3f894b93b0850626d71293fa3409d8ae66
mortendk <mortendk@book-royale-II.local>
2009-08-21 15:47:50 +0200

    cms sider venstre menu, alma user tpl genoprettet developer.css tilføjet til de hårdtarbejdende udviklere

---
+++ sites/all/themes/dynamo/alma_user_status_block.tpl.php
@@ -0,0 +1,50 @@
+<?php
+// $Id$
+
+/**
+ * @file alma_user_status_block.tpl.php
+ * Template for the user status block.
+ */
+?>
+<div id="account-profile" class="clearfix">
+	<div class="user">
+
+		<div class="logout">
+			<?php print l(t('log out'), 'logout', $options= array('attributes' => array('class' =>'logout')) );  ?>    
+		</div>
+
+		<h5><?php print t('Welcome'); ?></h5>
+		<div class="username">
+			<?php print l($user->name, 'patron/profile', $options= array('attributes' => array('class' =>'username')) );  ?>                                                
+		</div>
+
+	</div>
+
+	<?php if (!$user_status_unavailable){ ?>
+		<div class="cart">
+	    <div><?php print $cart_count; ?></div>
+	    <?php print l('Go to cart', 'patron/cart'); ?>
+		</div>
+
+		<ul>
+	    <li>
+	      <div class="content">
+					<?php print l(t("Loans") . ' <span class="count">' . $user_status['loan_count'] . '</span>', 'user/'. $user->uid . '/status', array('html' => TRUE)); ?>
+				</div>
+				<div class="status"><span class="warning">!!</span></div>
+	    </li>
+	    <li>
+				<div class="content">
+	      <?php print l(t("Reservations") . ' <span class="count">' . $user_status['reservation_count'] . '</span>', 'user/'. $user->uid . '/status', array('html' => TRUE)); ?>
+				</div>
+				<div class="status"><span class="ok">ok</span></div>
+	    </li>
+	</ul>
+	<?php }else{ ?>
+
+	  <div class="status-unavailable">
+	    <?php print $status_unavailable_message; ?>
+	  </div>
+	
+	<?php } ?>
+</div>
