a5f8f657bf5d3c91a1fe2a7acc9fdbead7554ec3
mortendk <mortendk@book-royale-II.local>
2009-08-24 13:09:11 +0200

    værker har fået links

--- sites/all/themes/dynamo/ting_object.tpl.php
+++ sites/all/themes/dynamo/ting_object.tpl.php
@@ -59,15 +59,15 @@
 					//we printed the first part up above so remove that 
 					unset($object->data->description[0]);
 					print theme('item_list',$object->data->description, NULL, 'div', array('class' => 'description'));?>	
-					<?php print theme('item_list',$object->data->identifier, t('identifier'), 'span', array('class' => 'identifier'));?>	
-					<?php print theme('item_list',$object->data->subject, t('subject'), 'span', array('class' => 'subject'));?>	
-					<?php print theme('item_list',$object->data->publisher, t('publisher'), 'span', array('class' => 'publisher'));?>						
-					<?php print theme('item_list',$object->data->type, t('type'), 'span', array('class' => 'type'));?>											
-					<?php print theme('item_list',$object->data->format, t('format'), 'span', array('class' => 'format'));?>											
-					<?php print theme('item_list',$object->data->language, t('language'), 'span', array('class' => 'language'));?>						
-					<?php print theme('item_list',$object->data->relation, t('relation'), 'span', array('class' => 'relation'));?>											
-					<?php print theme('item_list',$object->data->coverage, t('coverage'), 'span', array('class' => 'coverage'));?>											
-					<?php print theme('item_list',$object->data->rights, t('rights'), 'span', array('class' => 'rights'));?>						
+					<?php print theme('item_list',$object->data->identifier, t('Identifier'), 'span', array('class' => 'identifier'));?>	
+					<?php print theme('item_list',$object->data->subject, t('Subject'), 'span', array('class' => 'subject'));?>	
+					<?php print theme('item_list',$object->data->publisher, t('Publisher'), 'span', array('class' => 'publisher'));?>						
+					<?php print theme('item_list',$object->data->type, t('Type'), 'span', array('class' => 'type'));?>											
+					<?php print theme('item_list',$object->data->format, t('Format'), 'span', array('class' => 'format'));?>											
+					<?php print theme('item_list',$object->data->language, t('Language'), 'span', array('class' => 'language'));?>						
+					<?php print theme('item_list',$object->data->relation, t('Relation'), 'span', array('class' => 'relation'));?>											
+					<?php print theme('item_list',$object->data->coverage, t('Coverage'), 'span', array('class' => 'coverage'));?>											
+					<?php print theme('item_list',$object->data->rights, t('Rights'), 'span', array('class' => 'rights'));?>						
 				</div>
 
 				<div class="ding-box-wide">
