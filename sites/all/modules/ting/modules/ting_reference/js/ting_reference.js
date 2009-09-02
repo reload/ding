0476b74946eef80845a064a8b0925f227207570a
Christian Schmidt <christian@signaldigital.com>
2009-09-02 09:50:19 +0200

    autocomplete when site is installled in subdir.

--- sites/all/modules/ting/modules/ting_reference/js/ting_reference.js
+++ sites/all/modules/ting/modules/ting_reference/js/ting_reference.js
@@ -1,7 +1,7 @@
 Drupal.behaviors.addTingReferenceAutocomplete = function(context)
 {
 	type = jQuery('.ting-reference-type-radio:checked').val();
-	path = '/'+Drupal.settings.tingReference.autocomplete[type];
+	path = Drupal.settings.tingReference.autocomplete[type];
 	jQuery('input.ting-reference-autocomplete').each(function(i, e)
 	{
 		var autocompleter = jQuery(e);
@@ -15,7 +15,7 @@ Drupal.behaviors.addTingReferenceAutocomplete = function(context)
 		autocompleter.parents('td:first').find('input.ting-reference-type-radio').focus(function()
 		{
 			type = jQuery(this).val();
-			path = '/'+Drupal.settings.tingReference.autocomplete[type];
+			path = Drupal.settings.tingReference.autocomplete[type];
 			autocompleter.setOptions({ url: path });
 			autocompleter.flushCache();
 		});
