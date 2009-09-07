bf9fb9f638c1008a16324c7f30f7cc7c3322eaf6
Christian Schmidt <christian@signaldigital.com>
2009-09-07 18:21:17 +0200

    Also add url property in getObject()

--- sites/all/modules/ting/lib/facade/TingClientFacade.php
+++ sites/all/modules/ting/lib/facade/TingClientFacade.php
@@ -105,7 +105,6 @@ class TingClientFacade {
 		$types = array();
 		foreach ($collection->objects as &$object)
 		{
-			$object = self::addObjectUrl($object);
 			$types = array_merge($types, $object->data->type);
 		}
 		$collection->types = array_unique($types);
@@ -127,6 +126,7 @@ class TingClientFacade {
 		
 		foreach($objects as $object)
 		{
+			$object = self::addObjectUrl($object);
 			if (isset($object->data->identifier))
 			{
 				foreach ($object->data->identifier as $identifier)
