00d73628cdecdc84d1deb688bc19caaee74db6bf
Mikkel Hoegh <mikkel@hoegh.org>
2009-09-08 18:45:31 +0200

    Fixed IE scope bug + reformatted.
    Moved inclusion to footer.

--- sites/all/modules/ting/modules/ting_search_autocomplete/js/ting_search_autocomplete.js
+++ sites/all/modules/ting/modules/ting_search_autocomplete/js/ting_search_autocomplete.js
@@ -1,21 +1,23 @@
-Drupal.behaviors.addTingAutocomplete = function(context)
-{
-	path = Drupal.settings.tingSearchAutocomplete.path;
-	jQuery('input.ting-autocomplete').each(function(i, e)
-	{
-		var autocompleter = jQuery(e);
-		
-		autocompleter.autocomplete(path, 
-															{ scrollHeight: 200, /* Consider moving dimensions to CSS for easier customization */ 
-																width: 493,
-																delay: 200,
-																selectFirst: false,
-																matchCase: true,
-																formatResult: function(data) { return data[1]; }
-															}). result(function(event)
-															{
-																jQuery(event.target).addClass('ac_loading')
-																										.parents('form:first').submit();
-															});
-	});	
+/**
+ * JavaScript behavior for the autocomplete widget.
+ */
+Drupal.behaviors.addTingAutocomplete = function(context) {
+  $('input.ting-autocomplete').each(function () {
+    $(this)
+      .autocomplete(Drupal.settings.tingSearchAutocomplete.path, {
+        scrollHeight: 200, /* Consider moving dimensions to CSS for easier customization */
+        width: 493,
+        delay: 200,
+        selectFirst: false,
+        matchCase: true,
+        formatResult: function(data) { return data[1]; }
+      })
+      .result(function(event) {
+        $(event.target)
+          .addClass('ac_loading')
+          .parents('form:first')
+          .submit();
+      });
+  });
 }
+
