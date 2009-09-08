c172165ac4a1f1a6083840fb4fb75fc997dc06fc
mortendk <mortendk@book-royale-II.local>
2009-09-08 11:16:04 +0200

    calender block forside, logo

--- sites/all/themes/dynamo/views-view-fields--event-list--block.tpl.php
+++ sites/all/themes/dynamo/views-view-fields--event-list--block.tpl.php
@@ -1,8 +1,8 @@
 <!-- views-view-fields  event-list  BLOCK.tpl.php-->
 <?php 
   //converts the date value to time
-  $date = strtotime($fields['field_datetime_value']->raw);
-  if( date('d-m-Y') == date("d-m-Y", $date) ){
+  $date = strtotime($fields['field_datetime_value']->raw . 'Z');
+  if( date('d-m-Y') == format_date($date,'custom', "d-m-Y") ){
     $is_today = "today";
   }
 
@@ -18,18 +18,16 @@
 
   <div class="leaf <?php print $is_today ?>">
     <div class="day"><?php print format_date($date, 'custom', 'l');?></div>
-    <div class="date"><?php print date("j", $date);?></div>
+    <div class="date"><?php print format_date($date, 'custom', 'j');?></div>
     <div class="month"><?php print format_date($date, 'custom', 'M');?></div>
   </div>
 
   <div class="info">
     <?php print $fields['field_library_ref_nid']->content; ?>
     <h4><?php print $fields['title']->content; ?></h4>
-    <span class="time"><?php print date("h:i", $date); ?>  
-
-
+    <span class="time">
+			<?php print format_date($date, 'custom', 'h:i');?>
       <?php print $price; ?>
-    
     </span>
   </div>
 
