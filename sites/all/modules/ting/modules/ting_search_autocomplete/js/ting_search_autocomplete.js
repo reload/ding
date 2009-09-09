177c54537e1f77df8aaaa0071211defc840924e5
Rasmus Luckow-Nielsen <rasmus.luckow@gmail.com>
2009-09-09 11:18:20 +0200

    fikset IE6 bug på forsiden?

--- sites/all/modules/ting/modules/ting_search_autocomplete/js/ting_search_autocomplete.js
+++ sites/all/modules/ting/modules/ting_search_autocomplete/js/ting_search_autocomplete.js
@@ -5,13 +5,14 @@ Drupal.behaviors.addTingAutocomplete = function(context) {
   $('input.ting-autocomplete').each(function (index, domElement) {
     $(domElement)
       .autocomplete(Drupal.settings.tingSearchAutocomplete.path, {
-        scrollHeight: 200, /* Consider moving dimensions to CSS for easier customization */
+        scrollHeight: 200,
         width: 493,
         delay: 200,
         selectFirst: false,
         matchCase: true,
-        formatResult: function(data) { return data[1]; }
+        formatResult: function (data) { return data[1]; }
       })
+
       .result(function(event) {
         $(event.target)
           .addClass('ac_loading')
