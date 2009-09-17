1d096e798acea354b943d6659c3d0cf2db9a2d42
Christian Schmidt <christian@signaldigital.com>
2009-09-17 14:41:30 +0200

    Add node-topic.tpl.php

---
+++ sites/all/themes/dynamo/node-topic.tpl.php
@@ -0,0 +1,118 @@
+<?php
+/*
+  dsm(get_defined_vars());
+  dsm($node);
+  dsm($node->content);
+*/
+/*
+ad a class="" if we have anything in the $classes var
+this is so we can have a cleaner output - no reason to have an empty <div class="" id=""> 
+*/
+if($classes){
+   $classes = ' class="' . $classes . ' clearfix"';
+}
+
+if($id_node){
+  $id_node = ' id="' . $id_node . '"';  
+}
+?>
+
+<!-- node.tpl-->
+<?php if ($page == 0){ ?>
+<div<?php print $id_node . $classes; ?>>
+
+  <div class="picture">
+    <?php
+ 		if($field_image_rendered){
+			print $field_image_rendered; 	
+		}
+	?>
+  </div>
+
+  <div class="content">
+
+    <div class="subject">
+      <?php print return_terms_from_vocabulary($node, "1"); ?> 
+    </div>
+
+  	<?php if($node->title){	?>	
+      <h3><?php print l($node->title, 'node/'.$node->nid); ?></h3>
+  	<?php } ?>
+
+  	<div class="meta">
+  		<span class="time">
+  			<?php print format_date($node->created, 'custom', "j F Y") ?> 
+  		</span>	
+  		<span class="author">
+  			af <?php print theme('username', $node); ?>
+  		</span>	
+
+			<?php print $node->field_library_ref[0]['view'];  ?>
+
+			
+  		<?php if (count($taxonomy)){ ?>
+  		  <div class="taxonomy">
+  	   	  <?php print $terms ?> 
+  		  </div>  
+  		<?php } ?>
+  	</div>
+
+    <?php print $node->content['body']['#value'];?>
+    
+  </div>
+
+</div>
+<?php }else{ 
+//Content
+?>
+
+<div<?php print $id_node . $classes; ?>>
+
+  <div class="subject">
+    <?php print return_terms_from_vocabulary($node, "1"); ?> 
+  </div>
+
+	<?php if($node->title){	?>	
+	  <h1><?php print $title;?></h1>
+	<?php } ?>
+
+	<div class="meta">
+	  
+		<?php print format_date($node->created, 'custom', "j F Y - H:i") ?> 
+    <i><?php print t('by'); ?></i> 
+		<span class="author"><?php print theme('username', $node); ?></span>	
+	</div>
+
+	<div class="content">
+  <?php ?>
+    <?php foreach ($field_flexifield_topic as $field) { ?>
+      <?php if ($field['type'] == 'flexifield_links') { ?>
+        <ul>
+          <?php foreach ($field['value']['field_links'] as $link) { ?>
+            <li><?php print l($link['title'], $link['url']); ?></li>
+          <?php } ?>
+        </ul>
+      <?php } else { ?>
+        <?php print $field['view'] ?>
+      <?php } ?>
+    <?php } ?>
+	</div>
+
+	<?php if (count($taxonomy)){ ?>
+	  <div class="taxonomy">
+   	  <?php print $terms ?> 
+	  </div>  
+	<?php } ?>
+		
+
+  <?php $similar_nodes = similarterms_list(variable_get('ding_similarterms_vocabulary_id', 0)); ?>
+  <?php if (count($similar_nodes)) { ?>
+		<div class="ding-box-wide similar">		
+      <h3><?php print t('Similar'); ?></h3>
+      <?php print theme('similarterms', variable_get('similarterms_display_options', 'title_only'), $similar_nodes); ?>
+    </div>
+  <?php } ?>
+
+</div>
+<?php } ?>
+<!-- /node.tpl-->
