21ea699597b88e68473058ef52ab7a54753b0c66
kasperg <kasper@headinsky.dk>
2009-10-05 13:28:12 +0200

    Added maximum dimensions to ting search carousel info and creator and title as link title attribute

--- sites/all/modules/ting/modules/ting_search_carousel/ting_search_carousel_collection.tpl.php
+++ sites/all/modules/ting/modules/ting_search_carousel/ting_search_carousel_collection.tpl.php
@@ -1,7 +1,9 @@
 <li>
-	<a href="<?php echo $collection->url ?>">
+	<a href="<?php echo $collection->url ?>" title="<?php echo implode($collection->objects[0]->data->creator, ', ')?>: <?php echo implode($collection->objects[0]->data->title, ', ')?>">
 		<img src="<?php echo ting_search_cover_url($collection->objects[0], 'ting_search_carousel') ?>" />
-		<span class="creator"><?php echo implode($collection->objects[0]->data->creator, ',')?></span>
-		<span class="title"><?php echo implode($collection->objects[0]->data->title, ',')?></span>
+		<div class="info">
+			<span class="creator"><?php echo implode($collection->objects[0]->data->creator, ', ')?></span>
+			<span class="title"><?php echo implode($collection->objects[0]->data->title, ', ')?></span>
+	  </div>
 	</a>
 </li>
