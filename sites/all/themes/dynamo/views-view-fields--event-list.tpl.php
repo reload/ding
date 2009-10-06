96f691e8ad11e253c8827a28b0abab84e347cfda
Christian Schmidt <christian@signaldigital.com>
2009-10-06 17:06:33 +0200

    use list_image instead of image in event list.

--- sites/all/themes/dynamo/views-view-fields--event-list.tpl.php
+++ sites/all/themes/dynamo/views-view-fields--event-list.tpl.php
@@ -6,7 +6,7 @@
 <div class="node-teaser clearfix">
 
   <div class="picture">
-    <?php print $fields['field_image_fid']->content; ?>  
+    <?php print $fields['field_list_image_fid']->content; ?>
   </div>
 
   <div class="info">
