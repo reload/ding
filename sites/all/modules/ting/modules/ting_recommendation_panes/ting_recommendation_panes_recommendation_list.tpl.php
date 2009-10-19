086c1c4e0559950a1fa582503cd6a798112dad1d
kasperg <kasper@headinsky.dk>
2009-10-19 11:13:02 +0200

    Updated styling to add pane title and other minor adjustments

--- sites/all/modules/ting/modules/ting_recommendation_panes/ting_recommendation_panes_recommendation_list.tpl.php
+++ sites/all/modules/ting/modules/ting_recommendation_panes/ting_recommendation_panes_recommendation_list.tpl.php
@@ -1,11 +1,9 @@
 <?php if ($objects && (sizeof($objects) > 0)) :?>
-  <div class="ting-recommendation-list">
-	  <ul>
-	  <?php foreach ($objects as $i => $object) : ?>
-	    <li class="<?php echo (($i % 2) == 0) ? 'odd' : 'even' ?>">
-	      <?php echo theme('ting_recommendation_panes_recommendation_list_entry', $object); ?>      
-	    </li>    
-	  <?php endforeach; ?>
-	  </ul>
-	</div>
+	<ul class="ting-recommendation-list">
+	 <?php foreach ($objects as $i => $object) : ?>
+	   <li class="<?php echo (($i % 2) == 0) ? 'odd' : 'even' ?>">
+	     <?php echo theme('ting_recommendation_panes_recommendation_list_entry', $object); ?>      
+	   </li>    
+	 <?php endforeach; ?>
+	</ul>
 <?php endif; ?>
--- sites/all/themes/dynamo/ting_recommendation_panes_recommendation_list.tpl.php
+++ sites/all/themes/dynamo/ting_recommendation_panes_recommendation_list.tpl.php
@@ -1,13 +1,9 @@
 <?php if ($objects && (sizeof($objects) > 0)) :?>
-  <div class="ting-recommendation-list">
-    <div class="ding-box-wide">
-		  <ul>
-		  <?php foreach ($objects as $i => $object) : ?>
-		    <li class="<?php echo (($i % 2) == 0) ? 'odd' : 'even' ?> <?php echo ((is_int(($i-1) / 4)) || (is_int($i / 4))) ? 'either' : 'or' ; ?>">
-		      <?php echo theme('ting_recommendation_panes_recommendation_list_entry', $object); ?>      
-		    </li>    
-		  <?php endforeach; ?>
-		  </ul>
-		</div>
-	</div>
+  <ul class="ting-recommendation-list">
+	  <?php foreach ($objects as $i => $object) : ?>
+	    <li class="<?php echo (($i % 2) == 0) ? 'odd' : 'even' ?> <?php echo ((is_int(($i-1) / 4)) || (is_int($i / 4))) ? 'either' : 'or' ; ?>">
+	      <?php echo theme('ting_recommendation_panes_recommendation_list_entry', $object); ?>      
+	    </li>    
+	  <?php endforeach; ?>
+  </ul>
 <?php endif; ?>
