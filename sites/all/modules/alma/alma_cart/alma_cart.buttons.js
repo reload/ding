a89e6685923913ca23f1b66224993a14254e530c
Mikkel Hoegh <mikkel@hoegh.org>
2009-10-21 15:04:57 +0200

    Fix a few bugs for [#598].

--- sites/all/modules/alma/alma_cart/alma_cart.buttons.js
+++ sites/all/modules/alma/alma_cart/alma_cart.buttons.js
@@ -16,17 +16,23 @@ Drupal.behaviors.almaCartButtons = function () {
 
       // Make the request back to Drupal.
       $.post(this.href, {}, function (data) {
+        // Message is overwritten by the data attribute.
+        var message = '';
         var buttons = {};
-        if (data.status != 'success') {
-          buttons[Drupal.t('Close')] = function () { $(this).dialog('close'); };
+        buttons[Drupal.t('Close')] = function () { $(this).dialog('close'); };
+
+        if (data) {
+          message = data.message;
+          if (data.status == 'success') {
+            buttons[Drupal.t('View cart…')] = function () { window.location = data.cart_link };
+          }
         }
         else {
-          buttons[Drupal.t('Close')] = function () { $(this).dialog('close'); };
-          buttons[Drupal.t('View cart…')] = function () { window.location = data.cart_link };
+          message = t('An error occurred.');
         }
 
         $('#alma-cart-dialog')
-          .text(data.message)
+          .text(message)
           .dialog({'title': data.title, 'buttons': buttons, 'autoOpen': false})
           .dialog("open");
       }, 'json');
