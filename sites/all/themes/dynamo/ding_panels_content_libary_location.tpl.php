9fb1e39a4f6e030b8a5886aa8c720a425932ddc4
mortendk <mortendk@book-royale-II.local>
2009-11-26 14:03:42 +0100

    #809

--- sites/all/themes/dynamo/ding_panels_content_libary_location.tpl.php
+++ sites/all/themes/dynamo/ding_panels_content_libary_location.tpl.php
@@ -22,12 +22,16 @@
 	      <span class="locality"><?php print $node->location['city']; ?></span>
 	      <div class="country-name"><?php print $node->location['country_name']; ?></div>
 	    </div>
+      <?php if($node->location['phone']){ ?>
 	    <div class="tel">
 	      <span class="type"><?php print t('Phone'); ?></span> <?php print $node->location['phone']; ?>
 	    </div>
+	    <?php } ?>
+      <?php if($node->location['fax']){ ?>
 	    <div class="tel">
 	      <span class="type"><?php print t('Fax'); ?></span> <?php print $node->location['fax']; ?>
 	    </div>
+	    <?php } ?>
 	  </div>
 
 	  <?php print $library_links; ?>  
