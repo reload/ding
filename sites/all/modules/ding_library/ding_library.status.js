d749b01f63f54dc6f3c1e8c9986d75c13a4ab311
Mikkel Hoegh <mikkel@hoegh.org>
2009-11-30 23:13:39 +0100

    Lots of office hours fixes.

---
+++ sites/all/modules/ding_library/ding_library.status.js
@@ -0,0 +1,16 @@
+// $Id$
+
+/**
+ * @file
+ * JavaScript behavior to update library open/closed status dynamically.
+ */
+
+Drupal.behaviors.dingLibraryStatus = function () {
+  $.getJSON(Drupal.settings.officeHours[1].callback + '/' + Drupal.settings.dingLibraryNids.join(',') + '/' + Drupal.settings.officeHours[1].field_name, {}, function (response, textStatus) {
+    $.each(response.data, function (nid, hoursData) {
+      $('#node-' + nid + ' .libary-openstatus')
+        .text(hoursData.status_local);
+    });
+  });
+};
+
