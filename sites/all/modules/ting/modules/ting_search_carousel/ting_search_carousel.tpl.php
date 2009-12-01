c299f5395cba66e827a8f67773c6e032b2718ec8
kasperg <kasper@headinsky.dk>
2009-12-01 23:28:47 +0100

    Updated method for determining initial active search

--- sites/all/modules/ting/modules/ting_search_carousel/ting_search_carousel.tpl.php
+++ sites/all/modules/ting/modules/ting_search_carousel/ting_search_carousel.tpl.php
@@ -1,9 +1,19 @@
-<?php $keys = array_keys($searches); ?>
+<?php
+//determine first actual search - i.e. it has a title
+//system form can contain empty elements
+$found = false;
+$firstIndex = -1;
+while (!$found && ($search = next($searches)))
+{
+  $firstIndex++;
+	$found = (bool) $search['title'];
+}
+?>
 <div class="ting-search-carousel">
 	<ul class="search-results">
 		<?php foreach ($searches as $i => $search) :?>
 		  <?php if ($search['title']) : ?>
-				<li class="<?php echo ($i == $keys[0]) ? 'active' : ''; ?>">
+				<li class="<?php echo ($i == $firstIndex) ? 'active' : ''; ?>">
 					<div class="subtitle">
 						<?php echo $search['subtitle']; ?>
 					</div>							
@@ -17,7 +27,7 @@
 	<ul class="search-controller">
 		<?php foreach ($searches as $i => $search) : ?>
 		  <?php if ($search['title']) :?>
-				<li class="<?php echo ($i == $keys[0]) ? 'active' : ''; ?>">
+				<li class="<?php echo ($i == $firstIndex) ? 'active' : ''; ?>">
 					<a href="#"><?php echo $search['title'] ?></a>
 				</li>
 			<?php endif; ?>
