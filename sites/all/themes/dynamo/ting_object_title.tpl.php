113dae37328dae1de9207f1f4d49a5b76ebb44d2
Christian Schmidt <christian@signaldigital.com>
2009-12-09 17:05:40 +0100

    Move ting_object_title into tpl file.
    Moved into ting_search_theme() like other ting_* theme hooks (perhaps all these hooks some day should be moved back to ting_theme().
    
    [#849]

---
+++ sites/all/themes/dynamo/ting_object_title.tpl.php
@@ -0,0 +1,31 @@
+<?php
+// $Id$
+/**
+ * @file ting_object_title.tpl.php
+ *
+ * Template to render a Ting object title (including author etc.).
+ *
+ * Available variables:
+ * - $object: The TingClientObject instance we're rendering.
+ */
+?>
+<!-- ting_object_title.tpl -->
+<?php if ($display_image) { ?>
+  <?php $image_url = ting_covers_object_url($object, '80_x'); ?>
+  <?php if ($image_url) { ?>
+    <?php print theme('image', $image_url, '', '', NULL, FALSE); ?>
+  <?php } ?>
+<?php } ?>
+<span class="title"><?php print l($object->data->title[0], $object->url); ?></span>
+<span class='creator'>
+  <span class='byline'><?php echo t('by'); ?></span>
+  <?php
+  foreach ($object->data->creator as $i => $creator) {
+    if ($i) {
+      print ', ';
+    }
+    print check_plain($creator);
+  }
+  ?>
+  <span class='date'>(<?php echo $object->data->type[0]; ?>)</span>
+</div>
