cd53980fdeec2ff8f2a49163f1156e7a0e81b010
Christian Schmidt <christian@signaldigital.com>
2009-12-10 10:56:58 +0100

    Show "Title not available" for objects not in Ting
    [#849]

--- sites/all/themes/dynamo/ting_object_title.tpl.php
+++ sites/all/themes/dynamo/ting_object_title.tpl.php
@@ -10,22 +10,27 @@
  */
 ?>
 <!-- ting_object_title.tpl -->
-<?php if ($display_image) { ?>
-  <?php $image_url = ting_covers_object_url($object, '80_x'); ?>
-  <?php if ($image_url) { ?>
-    <?php print theme('image', $image_url, '', '', NULL, FALSE); ?>
+<?php if (!$object) { ?>
+  <?php print t('Title not available.'); ?>
+<?php } else { ?>
+  <?php if ($display_image) { ?>
+    <?php $image_url = ting_covers_object_url($object, '80_x'); ?>
+    <?php if ($image_url) { ?>
+      <?php print theme('image', $image_url, '', '', NULL, FALSE); ?>
+    <?php } ?>
   <?php } ?>
-<?php } ?>
-<span class="title"><?php print l($object->data->title[0], $object->url); ?></span>
-<span class='creator'>
-  <span class='byline'><?php echo t('by'); ?></span>
-  <?php
-  foreach ($object->data->creator as $i => $creator) {
-    if ($i) {
-      print ', ';
+  <span class="title"><?php print l($object->data->title[0], $object->url); ?></span>
+  <span class='creator'>
+    <span class='byline'><?php echo t('by'); ?></span>
+    <?php
+    foreach ($object->data->creator as $i => $creator) {
+      if ($i) {
+        print ', ';
+      }
+      print check_plain($creator);
     }
-    print check_plain($creator);
-  }
-  ?>
-  <span class='date'>(<?php echo $object->data->type[0]; ?>)</span>
-</div>
+    ?>
+    <span class='date'>(<?php echo $object->data->type[0]; ?>)</span>
+  </div>
+<?php } ?>
+
