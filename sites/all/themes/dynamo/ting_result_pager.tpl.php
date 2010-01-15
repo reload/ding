49b89c38b44edb184b7b50d7f32002685e16eec2
Mikkel Hoegh <mikkel@hoegh.org>
2010-01-15 22:18:10 +0100

    Committed Ting result pager template.

--- sites/all/themes/dynamo/ting_result_pager.tpl.php
+++ sites/all/themes/dynamo/ting_result_pager.tpl.php
@@ -1,17 +1,6 @@
 <div id="pager">
-
-	<div class="navwide"><a class="prev" href=""><?php echo t('Previous') ?></a></div>
-	<div class="nav"><a class="first" href=""><?php echo t('First') ?></a></div>
-
-	<div id="ting-result-pager">
-		<ul>
-			<li>
-				<a href="">Page number</a>
-			</li>
-		</ul>
-	</div>
-
-	<div class="nav"><a class="last" href=""><?php echo t('Last') ?></a></div>
-	<div class="navwide"><a class="next" href=""><?php echo t('Next') ?></a></p>
-
+	<div class="navwide"><a class="prev" href="#prev"><?php echo t('Previous') ?></a></div>
+	<div class="nav"><a class="first" href="#first"><?php echo t('First') ?></a></div>
+	<div class="navwide"><a class="next" href="#next"><?php echo t('Next') ?></a></p>
 </div>
+
