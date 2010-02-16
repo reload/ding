d90ffac53f7abf7c064017960671c99a4c731ebc
Mikkel Hoegh <mikkel@hoegh.org>
2010-02-16 13:04:00 +0100

    keys_api 6.x-1.2

--- sites/all/modules/contrib/keys_api/keys_api.js
+++ sites/all/modules/contrib/keys_api/keys_api.js
@@ -1,4 +1,4 @@
-// $Id: keys_api.js,v 1.3.4.2 2008/10/16 17:10:42 greenskin Exp $
+// $Id: keys_api.js,v 1.3.4.3 2010/01/06 18:21:56 greenskin Exp $
 
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
@@ -7,20 +7,5 @@ if (Drupal.jsEnabled) {
 
   function setDomain() {
     $("#edit-domain").val(window.location.host);
-  }
-
-  function deleteItem(item) {
-    $.each($("." + item + " td"), function(i, n) {
-      if (i == 0) {
-        domain = $(n).text();
-      }
-      if (i == 1) {
-        service = $(n).text();
-      }
-    });
-    
-    $.post(Drupal.settings.basePath + "?q=admin/settings/keys/delete_key",{name:domain,service:service},function(data) {
-      $("#keys_table").empty().append(data);
-    });
   }
 }
