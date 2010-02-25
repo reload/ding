e4f79b2a24aebdbfde940c3918f8cd24f0788cca
Christian Schmidt <christian@signaldigital.com>
2010-02-25 22:19:13 +0100

    Add teaser preview (based on node.tpl.php).

--- sites/all/themes/dynamo/node-article.tpl.php
+++ sites/all/themes/dynamo/node-article.tpl.php
@@ -14,6 +14,53 @@ if($id_node){
 ?>
 
 <!--node- article.tpl-->
+<?php if ($page == 0) { ?>
+<div <?php print $id_node . $classes; ?>>
+
+  <div class="picture"><?php print $list_image; ?></div>
+
+  <div class="content">
+
+    <div class="subject">
+      <?php print return_terms_from_vocabulary($node, "1"); ?>
+    </div>
+
+    <?php if($node->title) { ?>
+      <h3><?php print l($node->title, 'node/'.$node->nid); ?></h3>
+    <?php } ?>
+
+    <div class="meta">
+      <span class="time">
+        <?php print format_date($node->created, 'custom', "j F Y") ?>
+      </span>
+      <span class="author">
+        <?php print t('by') . ' ' . theme('username', $node); ?>
+      </span>
+
+      <?php print $node->field_library_ref[0]['view'];  ?>
+    </div>
+
+    <p>
+      <?php
+        if ($node->field_teaser[0]['value']) {
+          print $node->field_teaser[0]['value'];
+        }
+        else {
+          print strip_tags($node->content['body']['#value']);
+        }
+      ?>
+    </p>
+
+    <?php if (count($taxonomy)) { ?>
+      <div class="taxonomy">
+        <?php print $terms; ?>
+      </div>
+    <?php } ?>
+
+  </div>
+
+</div>
+<?php } else { ?>
 <div<?php print $id_node . $classes; ?>>
   <div class="subject">
     <?php print return_terms_from_vocabulary($node, "1"); ?> 
@@ -55,4 +102,5 @@ if($id_node){
 	<?php } ?>
 
 </div>
+<?php } ?> 
 <!-- /node.tpl-->
