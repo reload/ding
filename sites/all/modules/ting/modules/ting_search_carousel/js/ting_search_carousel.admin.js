a22ef0b0c41ba669b793e28d633f7bf43bbcce85
Jesper Kristensen <cableman@linuxdev.dk>
2010-04-23 16:21:43 +0200

    Fixed #1172: Change sorting in backend for " Materialekarrusel"

--- sites/all/modules/ting/modules/ting_search_carousel/js/ting_search_carousel.admin.js
+++ sites/all/modules/ting/modules/ting_search_carousel/js/ting_search_carousel.admin.js
@@ -3,47 +3,13 @@
 Drupal.behaviors.tingSearchCarouselAdmin = function(context)
 {
 	Drupal.tingSearchCarouselAdmin.remove();
-	Drupal.tingSearchCarouselAdmin.sort();
 };
 
 Drupal.tingSearchCarouselAdmin = {};
 
 Drupal.tingSearchCarouselAdmin.remove = function () {
-  $('.ting-search-carousel-search-wrap .remove').click(function (e) {
-	$(this).parents('.form-item').slideUp('normal', function () 
-	{ 
-	  searches = $(this).parents('#ting-search-carousels');
-	  $(this).remove();
-	  Drupal.tingSearchCarouselAdmin.updateIndexes(searches);
-  	});
-	return false;
+  $('.ting-search-carousel-search-wrap .remove').click(function () {
+    $(this).parents('tr').remove();
+    return false;
   });
 };
-
-Drupal.tingSearchCarouselAdmin.sort = function () {
-  $('#ting-search-carousels').sortable({
-	items: '.fieldset-content > *',
-	handle: '.sort',
-	axis: 'y',
-	update: function(event, ui) { Drupal.tingSearchCarouselAdmin.updateIndexes(ui.item.parents('#ting-search-carousels')); }
-  });
-};
-
-Drupal.tingSearchCarouselAdmin.updateIndexes = function(searches)
-{
-	var i = 0;
-	$(searches).find('.fieldset-content > .form-item').each(function()
-	{
-		
-		formElements = $(this).find('input[name^=ting_search_carousel_searches]');
-		if (formElements.size() > 0)
-		{
-			formElements.each(function()
-			{
-				name = $(this).attr('name').replace(/\[\d+\]/, '['+i+']');
-				$(this).attr('name', name);
-			});
-			i++;
-		}
-	 });
-}
